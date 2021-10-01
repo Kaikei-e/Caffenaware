dec6135514efcd18341836527e086bbea7702487
Kaikei-e <e_kaikei@protonmail.com>
2021-10-01 14:17:52 +0000

    Update: styling html and js

--- caffenaware/src/App.svelte
+++ caffenaware/src/App.svelte
@@ -1,34 +1,51 @@
 <script>
-	let count = 0;
-	function countUp(){
-		return count++
-	}
+  let drinkCount = 1;
+  function countUp() {
+    return drinkCount++;
+  }
 </script>
 
 <main>
   <div class="bg-img">
-		<div class="countNum theOpacity">{count}</div>
-		<button on:click={countUp}>Press me!!</button>
-	</div>
+    <div class="inputForm theOpacity">
+      <div class="countNum">How many bottles did you drink? : {drinkCount}</div>
+      <form class="firstForm">
+        <input type="number" maxlength="1" bind:value={drinkCount} />
+      </form>
+      <button class="countUp" on:click={countUp}> +1 drink :)</button>
+    </div>
+  </div>
 </main>
 
 <style>
-
   .bg-img {
-		background-image: url("../imgs/coffee2.jpg");
-		background-size: cover;
-		width: 1980px;
-		height: 1200px;
+    background-image: url("../imgs/coffee2.jpg");
+    background-size: cover;
+    width: 1980px;
+    height: 1200px;
   }
-	.countNum{
-		color: crimson;
-		font-size: large;
+  .countNum {
+    color: rgba(85, 52, 14, 0.8);
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .inputForm {
+    text-align: center;
+  }
+  .firstForm {
+    margin: 0px;
+    border-radius: 20px;
+  }
+	.countUp{
+    background-color: sienna;
+
 	}
 
-	.theOpacity{
-		background-color: rgb(143, 143, 143);
-    opacity: 0.8;
-	}
+  .theOpacity {
+    padding: 20px;
+    background-color: rgb(202, 202, 202);
+    opacity: 0.6;
+  }
 
   @media (min-width: 640px) {
     main {
