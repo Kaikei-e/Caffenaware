a895e47720d4ddac506e95a63ee3162a86e76d51
Kaikei-e <e_kaikei@protonmail.com>
2021-11-03 15:18:33 +0000

    Update: working on chart.js

--- caffenaware/src/routes/result.svelte
+++ caffenaware/src/routes/result.svelte
@@ -2,6 +2,8 @@
 	import { resStruct } from '$lib/api/apiSender';
 	import type { drinkFormRes } from '$lib/forms/formTypes';
 	import { get } from 'svelte/store';
+	import { Chart } from 'chart.js';
+	import { onMount } from 'svelte';
 
 	let jsonObjs = get(resStruct).res;
 	let drinks: Array<drinkFormRes> = [];
@@ -9,27 +11,52 @@
 	for (let index = 0; index < jsonObjs.length; index++) {
 		const element = jsonObjs[index];
 
-
 		let dStruct: drinkFormRes = {
-			caffeine: element["caffeine"],
-			dttm: element["dttm"],
-		}
-		drinks.push(dStruct)
-		
+			caffeine: element['caffeine'],
+			dttm: element['dttm']
+		};
+		drinks.push(dStruct);
 	}
+
+	let ctx;
+	let chartValues = [];
+	let chartLabels = [];
+
+	for (let index = 0; index < drinks.length; index++) {
+		const element = drinks[index];
+
+		chartValues.push(element.caffeine);
+		chartLabels.push(element.dttm);
+	}
+
+	let chartCanvas;
+
+	onMount(async () => {});
+  function renderChart() {
+    let ctx = document.getElementById("myChart").getContext("2d");
+    let chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }
+        ]
+      },
+      options: {}
+    });
+  }
 </script>
 
-<div class="backImage" />
-
-<div>
-	{#each drinks as drink, index}
-		{#if index % 500 == 0}
-			<div>{drink.caffeine}</div>
-			<div>{drink.dttm}</div>
-
-		{/if}		
-	{/each}
-</div>
+<main>
+	<div class="backImage">
+		<canvas id="myChart" />
+	</div>
+</main>
 
 <style>
 	:global(body) {
