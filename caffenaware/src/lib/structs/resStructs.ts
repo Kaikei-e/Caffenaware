b4ccae4b77e04d448e0515fab61382156d6e2207
Kaikei-e <e_kaikei@protonmail.com>
2021-11-05 15:43:10 +0000

    Update: prototyping Carbon design syste.

---
+++ caffenaware/src/lib/structs/resStructs.ts
@@ -0,0 +1,6 @@
+
+export type drawType = {
+  "group": string,
+  "value" : number,
+  "date": string
+}
--- caffenaware/src/routes/result.svelte
+++ caffenaware/src/routes/result.svelte
@@ -3,40 +3,57 @@
 	import type { drinkFormRes } from '$lib/forms/formTypes';
 	import { get } from 'svelte/store';
 
-	//import { Chart, ChartComponent, registerables } from 'chart.js';
-	import Line from "svelte-chartjs/src/Line.svelte"
-	import { Chart, registerables } from "chart.js";
+	//import { Chart, registerables } from 'chart.js';
 
-	//import 'chartjs-adapter-moment'; // or another adapter to avoid moment
-	Chart.register(...registerables);
+	//Chart.register(...registerables);
 
-	import '@carbon/charts/styles.min.css';
-	import 'carbon-components/css/carbon-components.min.css';
+	//import '@carbon/charts/styles.min.css';
+	//import 'carbon-components/css/carbon-components.min.css';
 	import { LineChart } from '@carbon/charts-svelte';
+	import type { drawType } from '$lib/structs/resStructs';
 
 	let ctx;
+	let chartGroup = [];
 	let chartValues = [];
 	let chartLabels = [];
+
+	let dType: drawType[] = [];
 
 	let jsonObjs = get(resStruct).res;
 	let drinks: Array<drinkFormRes> = [];
 
-	for (let index = 0; index < jsonObjs.length; index++) {
-		const element = jsonObjs[index];
+	function converter() {
+		for (let index = 0; index < jsonObjs.length; index++) {
+			const element = jsonObjs[index];
 
-		let dStruct: drinkFormRes = {
-			caffeine: element['caffeine'],
-			dttm: element['dttm']
-		};
-		drinks.push(dStruct);
+			let dStruct: drinkFormRes = {
+				caffeine: element['caffeine'],
+				dttm: element['dttm']
+			};
+			drinks.push(dStruct);
+		}
+
+		for (let index = 0; index < drinks.length; index++) {
+			const element = drinks[index];
+
+			dType.push({
+				group: 'Caffeine',
+				date: element.dttm,
+				value: element.caffeine
+			});
+		}
+
+	  console.log(
+			dType.forEach((val) => {
+				console.log(val.value);
+				console.log(val.date);
+			})
+		);
+
+		return;
 	}
 
-	for (let index = 0; index < drinks.length; index++) {
-		const element = drinks[index];
-
-		chartValues.push(element.caffeine);
-		chartLabels.push(element.dttm);
-	}
+	converter();
 
 	///////////////////////////////////////////////////
 	/*
@@ -63,189 +80,196 @@
 		}
 	});
 	//})
-*/
 
 	let dataLine = {
 		labels: chartLabels,
 		datasets: [
 			{
-				label: "Caffeine", 
+				label: 'Caffeine',
 				fill: true,
 				lineTension: 0.3,
-				backgroundColor: "rgba(225, 204,230, .3)",
-        borderColor: "rgb(205, 130, 158)",
-        borderCapStyle: "butt",
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgb(205, 130,1 58)",
-        pointBackgroundColor: "rgb(255, 255, 255)",
-        pointBorderWidth: 10,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgb(0, 0, 0)",
-        pointHoverBorderColor: "rgba(220, 220, 220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
+				backgroundColor: 'rgba(225, 204,230, .3)',
+				borderColor: 'rgb(205, 130, 158)',
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: 'rgb(205, 130,1 58)',
+				pointBackgroundColor: 'rgb(255, 255, 255)',
+				pointBorderWidth: 10,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+				pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10,
 				data: chartValues
 			}
 		]
-	}
+	};
 
-
+*/
 </script>
 
-<main>
+<main class="h-screen w-screen">
 	<div class="backImage">
-
-
 		<!--- 
+
 		<canvas bind:this={canvasChart} id="chart" />
 		<Line data={dataLine} options={{responsive: true}}/>
 
+		--->
 
-		<LineChart data={[{ group: 'Caffeine', key: chartLabels, value: chartValues }]} 
-		options={{
-		"title": "Line (discrete)",
-		"axes": {
-			"bottom": {
-				"title": "2019 Annual Sales Figures",
-				"mapsTo": "key",
-				"scaleType": "labels"
-			},
-			"left": {
-				"mapsTo": "value",
-				"title": "Conversion rate",
-				"scaleType": "linear"
-			}
-		},
-		"height": "400px"
-	}}/>
-	
+		<LineChart
+			data={dType}
+			options={{
+				title: 'Line (discrete)',
+				axes: {
+					bottom: {
+						title: '2019 Annual Sales Figures',
+						mapsTo: 'key',
+						scaleType: 'labels'
+					},
+					left: {
+						mapsTo: 'value',
+						title: 'Conversion rate',
+						scaleType: 'linear'
+					}
+				},
+
+				curve: 'curveMonotoneX',
+				height: '800px'
+			}}
+		/>
+
+		<!--- 
+
+		<div class="m-5">
+			<LineChart
+				data={[
+					{
+						group: 'Dataset 1',
+						key: 'Qty',
+						value: 34200
+					},
+					{
+						group: 'Dataset 1',
+						key: 'More',
+						value: 23500
+					},
+					{
+						group: 'Dataset 1',
+						key: 'Sold',
+						value: 53100
+					},
+					{
+						group: 'Dataset 1',
+						key: 'Restocking',
+						value: 42300
+					},
+					{
+						group: 'Dataset 1',
+						key: 'Misc',
+						value: 12300
+					},
+					{
+						group: 'Dataset 2',
+						key: 'Qty',
+						value: 34200
+					},
+					{
+						group: 'Dataset 2',
+						key: 'More',
+						value: 53200
+					},
+					{
+						group: 'Dataset 2',
+						key: 'Sold',
+						value: 42300
+					},
+					{
+						group: 'Dataset 2',
+						key: 'Restocking',
+						value: 21400
+					},
+					{
+						group: 'Dataset 2',
+						key: 'Misc',
+						value: 0
+					},
+					{
+						group: 'Dataset 3',
+						key: 'Qty',
+						value: 41200
+					},
+					{
+						group: 'Dataset 3',
+						key: 'More',
+						value: 18400
+					},
+					{
+						group: 'Dataset 3',
+						key: 'Sold',
+						value: 34210
+					},
+					{
+						group: 'Dataset 3',
+						key: 'Restocking',
+						value: 1400
+					},
+					{
+						group: 'Dataset 3',
+						key: 'Misc',
+						value: 42100
+					},
+					{
+						group: 'Dataset 4',
+						key: 'Qty',
+						value: 22000
+					},
+					{
+						group: 'Dataset 4',
+						key: 'More',
+						value: 1200
+					},
+					{
+						group: 'Dataset 4',
+						key: 'Sold',
+						value: 9000
+					},
+					{
+						group: 'Dataset 4',
+						key: 'Restocking',
+						value: 24000,
+						audienceSize: 10
+					},
+					{
+						group: 'Dataset 4',
+						key: 'Misc',
+						value: 3000,
+						audienceSize: 10
+					}
+				]}
+				options={{
+					title: 'Line (discrete)',
+					axes: {
+						bottom: {
+							title: '2019 Annual Sales Figures',
+							mapsTo: 'key',
+							scaleType: 'labels'
+						},
+						left: {
+							mapsTo: 'value',
+							title: 'Conversion rate',
+							scaleType: 'linear'
+						}
+					},
+					height: '400px'
+				}}
+			/>
+		</div>
+
 	--->
-
-
-<LineChart
-data={[
-{
-	"group": "Dataset 1",
-	"key": "Qty",
-	"value": 34200
-},
-{
-	"group": "Dataset 1",
-	"key": "More",
-	"value": 23500
-},
-{
-	"group": "Dataset 1",
-	"key": "Sold",
-	"value": 53100
-},
-{
-	"group": "Dataset 1",
-	"key": "Restocking",
-	"value": 42300
-},
-{
-	"group": "Dataset 1",
-	"key": "Misc",
-	"value": 12300
-},
-{
-	"group": "Dataset 2",
-	"key": "Qty",
-	"value": 34200
-},
-{
-	"group": "Dataset 2",
-	"key": "More",
-	"value": 53200
-},
-{
-	"group": "Dataset 2",
-	"key": "Sold",
-	"value": 42300
-},
-{
-	"group": "Dataset 2",
-	"key": "Restocking",
-	"value": 21400
-},
-{
-	"group": "Dataset 2",
-	"key": "Misc",
-	"value": 0
-},
-{
-	"group": "Dataset 3",
-	"key": "Qty",
-	"value": 41200
-},
-{
-	"group": "Dataset 3",
-	"key": "More",
-	"value": 18400
-},
-{
-	"group": "Dataset 3",
-	"key": "Sold",
-	"value": 34210
-},
-{
-	"group": "Dataset 3",
-	"key": "Restocking",
-	"value": 1400
-},
-{
-	"group": "Dataset 3",
-	"key": "Misc",
-	"value": 42100
-},
-{
-	"group": "Dataset 4",
-	"key": "Qty",
-	"value": 22000
-},
-{
-	"group": "Dataset 4",
-	"key": "More",
-	"value": 1200
-},
-{
-	"group": "Dataset 4",
-	"key": "Sold",
-	"value": 9000
-},
-{
-	"group": "Dataset 4",
-	"key": "Restocking",
-	"value": 24000,
-	"audienceSize": 10
-},
-{
-	"group": "Dataset 4",
-	"key": "Misc",
-	"value": 3000,
-	"audienceSize": 10
-}
-]}
-options={{
-"title": "Line (discrete)",
-"axes": {
-	"bottom": {
-		"title": "2019 Annual Sales Figures",
-		"mapsTo": "key",
-		"scaleType": "labels"
-	},
-	"left": {
-		"mapsTo": "value",
-		"title": "Conversion rate",
-		"scaleType": "linear"
-	}
-},
-"height": "400px"
-}} />
 	</div>
 </main>
 
