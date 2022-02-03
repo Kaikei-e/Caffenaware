b95bb983a26cd5a5f2b5e8d916768688c6eaddcd
Kaikei-e <e_kaikei@protonmail.com>
2022-02-03 23:28:57 +0900

    Update: fmt projet

--- goPkg/calculator/calSumTrans.go
+++ goPkg/calculator/calSumTrans.go
@@ -22,49 +22,5 @@ func CalSumTrans(caffeList []structs.TheDecay) []structs.TheDecay {
 		summedList = append(summedList, tmp)
 	}
 
-	/*
-
-
-			for i := 0; i < len(caffeList); i +=2 {
-		  	caffeMap[caffeList[i].Timeline] = caffeList[i+1].CaffeineTransition
-			}
-
-			for i, v := range caffeMap {
-				var tmp structs.TheDecay
-
-				tmp.CaffeineTransition = v
-				tmp.Timeline = i
-
-				summedList = append(summedList, tmp)
-			}
-		/*
-			for i, v := range caffeMap {
-
-
-			}
-	*/
-
-	/*
-		for _, v := range caffeList {
-			_, ok := duplicate[v.Timeline]
-			if ok {
-
-
-				/*
-				var sum structs.TheDecay
-
-				sum.CaffeineTransition = v.CaffeineTransition
-				sum.Timeline = v.Timeline
-
-
-				summedList = append(summedList, sum)
-			}else{
-				duplicate[v] = v
-			}
-
-
-		}
-	*/
-
 	return summedList
 }
